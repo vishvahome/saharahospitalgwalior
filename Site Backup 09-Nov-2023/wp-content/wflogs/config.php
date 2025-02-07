62fb7e400465d15268bf65812476ba28b9868fce
Vivek Soni <vivek.soni@gaurish.com>
2025-02-07 11:10:10 +0530

    Remove old backup

--- Site Backup 09-Nov-2023/wp-content/wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1700050140;s:7:"authKey";s:64:"^&ZyxkwLS$2YdS$vpSn;Xl]5~PGrX!!PAAg5%{rb22O6VlO/mIVSGEp3BJSF!)0!";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:3366;}
